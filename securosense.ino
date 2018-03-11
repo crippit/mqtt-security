3fbe8f6d520b1574001fe9937896eae6bb07263b
crippit <crippit@gmail.com>
2018-03-11 14:56:43 -0400

    Update securosense.ino

--- securosense.ino
+++ securosense.ino
@@ -2,19 +2,25 @@
 #include <PubSubClient.h>
 
 // this constant won't change:
-const int buttonPin = D2;   // the pin that the door is attached to
-const int door2Pin = D3;    // the pin that door 2 is attached to     
-const int door3Pin = D6;    // the pin that door 3 is attached to
-const int ledPin = D4;       // the pin that the LED is attached to
+const int door1Pin = D1;   // the pin that the door is attached to
+const int door2Pin = D2;    // the pin that door 2 is attached to     
+const int door3Pin = D3;    // the pin that door 3 is attached to
+const int window1Pin = D5;  // the pin that window 1 is attached to
+const int window2Pin = D6;  // the pin that window 2 is attached to
+const int ledPin = D4;      // the pin that the LED is attached to
 
 // Current State:
-int buttonState = 0; // current state of the button
+int door1State = 0; 
 int door2State = 0;
 int door3State = 0;
+int window1State = 0;
+int window2State = 0;
 //Last State
-int lastButtonState = 0;     // previous state of the button
+int lastDoor1State = 0;     
 int lastDoor2State = 0;
 int lastDoor3State = 0;
+int lastWindow1State = 0;
+int lastWindow2State = 0;
 
 //WiFi & MQTT 
 const char* ssid = "SSID;
@@ -31,9 +37,11 @@ int value = 0;
 
 void setup() {
   // initialize the button pin as a input:
-  pinMode(buttonPin, INPUT_PULLUP);
+  pinMode(door1Pin, INPUT_PULLUP);
   pinMode(door2Pin, INPUT_PULLUP);
   pinMode(door3Pin, INPUT_PULLUP);
+  pinMode(window1Pin, INPUT_PULLUP);
+  pinMode(window2Pin, INPUT_PULLUP);
   // initialize the LED as an output:
   pinMode(ledPin, OUTPUT);
   // initialize serial communication:
@@ -95,16 +103,20 @@ void loop() {
     setup_wifi();
   }
   // read the pushbutton input pin:
-  buttonState = digitalRead(buttonPin);
+  door1State = digitalRead(door1Pin);
   door2State = digitalRead(door2Pin);
   door3State = digitalRead(door3Pin);
+  window1State = digitalRead(window1Pin);
+  window2State = digitalRead(window2Pin);
   //Door 1 = Front Door
   //Door 2 = Back Door
   //Door 3 = Garage Door
+  //Window 1 =
+  //Window 2 =
   // compare the door to its previous state
-    if (buttonState != lastButtonState) {
+    if (door1 != lastDoor1State) {
     // if the door state has changed
-    if (buttonState == LOW) {
+    if (door1State == LOW) {
             Serial.println("Door Open");
             client.publish("ha/door/door1","Open");
             Serial.println("Published Door 1 Open");
@@ -116,7 +128,7 @@ void loop() {
     // Delay a little bit to avoid bouncing
     delay(50);
   }
-   lastButtonState = buttonState;
+   lastDoor1State = door1State;
 
   //##### Door2 #######
   if (door2State != lastDoor2State) {
@@ -152,4 +164,39 @@ void loop() {
   // save the current state as the last state, for next time through the loop
   lastDoor3State = door3State;
 
+    //##### Window 1 ######
+  if (window1State != lastWindow1State) {
+  // if the state has changed, increment the counter
+    if (window1State == LOW) {
+      Serial.println("Window 1 Open");
+      client.publish("ha/window/window1","Open");
+      digitalWrite(ledPin, HIGH);
+          } else {
+      Serial.println("Door 3 Closed");
+      client.publish("ha/window/window1","Closed");
+      digitalWrite(ledPin, LOW);
+    }
+    // Delay a little bit to avoid bouncing
+    delay(50);
+  }
+  // save the current state as the last state, for next time through the loop
+  lastWindow1State = window1State;
+  
+    //##### Window 2 ######
+  if (window2State != lastWindow2State) {
+  // if the state has changed, increment the counter
+    if (window2State == LOW) {
+      Serial.println("Window 2 Open");
+      client.publish("ha/window/window2","Open");
+      digitalWrite(ledPin, HIGH);
+          } else {
+      Serial.println("Window 2 Closed");
+      client.publish("ha/window/window2","Closed");
+      digitalWrite(ledPin, LOW);
+    }
+    // Delay a little bit to avoid bouncing
+    delay(50);
+  }
+  // save the current state as the last state, for next time through the loop
+  lastWindow2State = window2State;
 }
